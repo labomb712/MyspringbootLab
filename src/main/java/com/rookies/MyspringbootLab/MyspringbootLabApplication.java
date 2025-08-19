624613fe8b61ef309fa93eefa9777f463d97fd1e
labomb712 <ginong00@gmail.com>
2025-08-19 21:21:02 +0900

    실습2-3

--- src/main/java/com/rookies/MyspringbootLab/MyspringbootLabApplication.java
+++ src/main/java/com/rookies/MyspringbootLab/MyspringbootLabApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyspringbootLabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootLabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringbootLabApplication.class, args);
+    }
 
 }
